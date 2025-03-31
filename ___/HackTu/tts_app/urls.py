3e198bb344e1c85a1e321c19b82c0252ac10ed79
arnav1827 <146094961+arnav1827@users.noreply.github.com>
2025-03-31 20:17:15 +0530

    Delete ___ directory

--- ___/HackTu/tts_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import speech_to_text, text_to_speech
-
-urlpatterns = [
-    path("speech-to-text/", speech_to_text, name="speech-to-text"),
-    path("text-to-speech/", text_to_speech, name="text-to-speech"),
-]
